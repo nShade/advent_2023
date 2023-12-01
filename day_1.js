aa4c77055ce1640821c0ec58819e3aa671240d7a
Ilya Karpeev <karpeev@fastmail.com>
2023-12-01 19:13:25 +0100

    Day 1 part 2 improve algorithm

--- day_1.js
+++ day_1.js
@@ -4,7 +4,8 @@ fs.readFile('input_day_1.txt', 'utf-8', (err, data) => {
     if (err) {
         console.error(err);
         return;
-    }
+    };
+
     let data_arr = data.split('\r\n');
 
     let res = data_arr.map(findFirstAndLastDigit).reduce(sum);
@@ -12,7 +13,6 @@ fs.readFile('input_day_1.txt', 'utf-8', (err, data) => {
 
     let res2 = data_arr.map(findFirstAndLastDigit2).reduce(sum);
     console.log(res2);
-
 })
 
 function sum(a, b) {
@@ -31,39 +31,34 @@ function findFirstAndLastDigit(s){
     return + (first_digit + last_digit);
 }
 
+const number_names = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const number_lenths = number_names.map(x => x.length)
+const number_letters = number_names.map(x => x.split(''));
+
 function findFirstAndLastDigit2(s){
     let first_digit;
     let last_digit;
-    let numbers = {
-        one: "1",
-        two: "2",
-        three: "3",
-        four: "4",
-        five: "5",
-        six: "6",
-        seven: "7",
-        eight: "8",
-        nine: "9",
-        zero: "0"
-    };
+    let matches = number_names.map(x => 0);
 
-    [" "].concat(s.split('')).reduce((total, value, index, array) => {
-        let last_five = array.slice(index > 3 ? index - 4 : 0, index + 1);        
+    [" "].concat(s.split('')).reduce((_, char) => {
+        matches = matches.map((match, index) => {
+            return (char == number_letters[index][match]) ? match + 1 : (char == number_letters[index][0]) ? 1 : 0
+        });
 
-        value = numbers[last_five.join("")] ?? 
-                numbers[last_five.slice(1).join("")] ?? 
-                numbers[last_five.slice(2).join("")] ?? 
-                value;
+        let match_index = matches.findIndex((value, index) => {return value == number_lenths[index]});
+        matches[match_index] = 0;
+        char = numbers[match_index] ?? char;
 
+        if (isDigit(char)) {
+            first_digit = first_digit ?? char;
+            last_digit = char;
+        };
+    });
 
-        if (isDigit(value)) {
-            first_digit = first_digit ?? value;
-            last_digit = value;
-        }
-    })
     return + (first_digit + last_digit);
 }
 
 function isDigit(c) {
-    return c >= '0' && c <= '9';
+    return Number.isFinite(Number(c));
 }
