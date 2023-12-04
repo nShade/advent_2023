ce9b74f7cc077fc50f1957259a2ad3eac8f77496
Ilya Karpeev <karpeev@fastmail.com>
2023-12-04 11:01:00 +0100

    Simplify code day 4

--- day_4.js
+++ day_4.js
@@ -11,8 +11,8 @@ fs.readFile('input_day_4.txt', 'utf-8', (err, data) => {
                         return x.split(':')[1]
                                 .split('|')
                                 .map(y => {return y.split(' ')
-                                                   .filter(z => {return !(z == '')})
-                                                   .map(m=> {return Number(m)})})
+                                                   .filter(z => !(z == ''))
+                                                   .map(Number)});
                     });
 
     let res = sum_winning(cards);
@@ -24,26 +24,22 @@ fs.readFile('input_day_4.txt', 'utf-8', (err, data) => {
 
 function sum_winning(cards){
     return cards.map(([on_card, winning]) => {
-        let have_winning = on_card.filter(n => {return winning.includes(n)}).length;
+        let have_winning = on_card.filter(n => winning.includes(n)).length;
         return have_winning == 0 ? 0 : 2 ** (have_winning - 1);
     }).reduce(sum, 0);
 }
 
 function count_cards(cards){
-    let cards_count = cards.map(x=> {return 1});
+    let cards_count = cards.map(x => 1);
 
-    let cards_winning = cards.map(([on_card, winning]) => {
-        return on_card.filter(n => {return winning.includes(n)}).length;
-
-    })
-
-    cards_winning.forEach((winning, index) => {
-        for (let i = index + 1; (i < index + winning + 1) && (i < cards_count.length); i++){
+    cards.forEach(([on_card, winning], index) => {
+        let have_winning = on_card.filter(n => winning.includes(n)).length;
+        for (let i = index + 1; (i < index + have_winning + 1) && (i < cards_count.length); i++){
             cards_count[i] += cards_count[index];
-        }
+        }        
     });
 
     return cards_count.reduce(sum, 0);
 }
 
-sum = (a, b)=> {return a + b};
+sum = (a, b)=> a + b;
