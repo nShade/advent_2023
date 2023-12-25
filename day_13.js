66dc693c8e9c5c2d1c97654b8f6a24cacc43e66d
Ilya Karpeev <karpeev@fastmail.com>
2023-12-25 07:42:49 +0100

    Add day 13 part 2

--- day_13.js
+++ day_13.js
@@ -8,28 +8,27 @@ fs.readFile('input_day_13.txt', 'utf-8', (err, data) => {
     };
 
     let patterns = data.split('\r\n\r\n').map(x => x.split('\r\n').map(x => x.split('')));
-    // console.log(patterns);
 
-    // pat = [[".", "#", "#"], ["#", "#", "#"]];
-
-    // console.log(mirror(pat))
-    // patterns.reduce((res, pat) => res + mirror(pat), 0);
-    // patterns.reduce((res, pat) => res + 100 * mirror(transpose(pat)), 0)
-
-    let res =
-        patterns.reduce((res, pat) => res + mirror(pat), 0) +
-        patterns.reduce((res, pat) => res + 100 * mirror(transpose(pat)), 0);
+    let res = patterns.reduce((res, pat) => res + mirror(pat, symmetrical), 0);
     console.log(res);
 
-    let res2;
+    let res2 = patterns.reduce((res, pat) => res + mirror(pat, smudgedSymmetrical), 0);
     console.log(res2);
 });
 
 
-function mirror(pattern) {
+function mirror(pattern, fn_symmetrical) {
     for (let i = 1; i < pattern[0].length; i++) {
-        if (symmetrical(pattern, i)) {
+        if (fn_symmetrical(pattern, i)) {
             return i;
+        }
+    }
+
+    let transposed_pattern = transpose(pattern);
+
+    for (let i = 1; i < pattern.length; i++) {
+        if (fn_symmetrical(transposed_pattern, i)) {
+            return 100 * i;
         }
     }
     return 0;
@@ -46,6 +45,23 @@ let symmetrical = (pattern, line_of_symmetry) => {
         }
     }
     return true;
+}
+
+let smudgedSymmetrical = (pattern, line_of_symmetry) => {
+    let start = Math.max(2 * line_of_symmetry - pattern[0].length, 0);
+    let smudges = 0;
+
+    for (let row of pattern) {
+        for (let j = start; j < line_of_symmetry; j++) {
+            if (row[j] != row[2 * line_of_symmetry - 1 - j]) {
+                smudges += 1;
+                if (smudges > 1) {
+                    return false;
+                }
+            }
+        }
+    }
+    return smudges == 1;
 }
 
 function transpose(matrix) {
