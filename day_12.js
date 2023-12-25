3c4c9af96cea891bcca0ad470b2e6405682b3aa0
Ilya Karpeev <karpeev@fastmail.com>
2023-12-25 01:29:16 +0100

    Update day 12 optimize

--- day_12.js
+++ day_12.js
@@ -9,63 +9,88 @@ fs.readFile('input_day_12.txt', 'utf-8', (err, data) => {
 
     let springs_data = data.split('\r\n').map(line => {
         let [spring_map, damaged_groups] = line.split(' ');
-        return [spring_map.split(''), damaged_groups.split(',').map(Number)]; 
+        return [spring_map, damaged_groups.split(',').map(Number)];
     });
 
-    let res = springs_data.map(spring_data => findNumberArrangements(...spring_data)).reduce((a, b) => a + b);
+    let res = springs_data.map(spring_data => possibleArrangements(...spring_data)).reduce((a, b) => a + b);
     console.log(res);
 
-    // let unfolded_springs_data = springs_data.map(([spring_map, damaged_groups]) => {
-    //     let resulting_spring_map = spring_map;
-    //     let resulting_damaged_groups = damaged_groups;
-    //     for (let i=0; i< 4; i++){
-    //         resulting_spring_map = resulting_spring_map.concat(['?']).concat(spring_map);
-    //         resulting_damaged_groups = resulting_damaged_groups.concat(damaged_groups);
-    //     }
-    //     return [resulting_spring_map, resulting_damaged_groups];
-    // });
-    // let res2 = unfolded_springs_data.map(spring_data => findNumberArrangements(...spring_data)).reduce((a, b) => a + b);
+    let unfolded_springs_data = springs_data.map(([spring_map, damaged_groups]) => {
+        let resulting_spring_map = spring_map;
+        let resulting_damaged_groups = damaged_groups;
+        for (let i = 0; i < 4; i++) {
+            resulting_spring_map = resulting_spring_map + '?' + spring_map;
+            resulting_damaged_groups = resulting_damaged_groups.concat(damaged_groups);
+        }
+        return [resulting_spring_map, resulting_damaged_groups];
+    });
+    let startTime = performance.now();
     let res2 = 0;
+    unfolded_springs_data.forEach((spring_data, index) => {
+        res2 += possibleArrangements(...spring_data);
+        console.log(index);
+    });
+    let endTime = performance.now();
+    console.log(endTime - startTime);
     console.log(res2);
 });
 
-function findNumberArrangements(spring_map, damaged_groups){
-    return possibleArrangements(damaged_groups, spring_map.length).filter(arrangement => fitsMap(arrangement, spring_map)).length
-}
+function possibleArrangements(springs_map, groups) {
+    let arrangements = 0;
+    let [group, ...remaining] = groups;
+    let max_pos = springs_map.length - sum(groups) - groups.length + 1;
 
-function fitsMap(arrangement, spring_map){
-    for (let [index, spring] of arrangement.entries()) {
-        if (spring_map[index] != '?' && spring_map[index] != spring){
-            return false;
+    for (let pos = 0; pos <= max_pos; pos++) {
+        if (!operational(springs_map, 0, pos) ||
+            !damaged(springs_map, pos, pos + group)) {
+            continue;
         }
-    };
-    return true;
-}
 
-function possibleArrangements(damaged_groups, map_length){
-    let arrangements = [];
-    let max_group_pos = map_length - damaged_groups.reduce((a, b) => a + b, 0) - damaged_groups.length + 1;
-    for (let first_group_pos = 0; first_group_pos <= max_group_pos; first_group_pos++){
-        let arrangement = [];
-        for (let i=0; i< first_group_pos; i++){
-            arrangement.push('.');
-        }
-        for (let i=first_group_pos; i< first_group_pos + damaged_groups[0]; i++){
-            arrangement.push('#');
-        }
-        let remaining = damaged_groups.slice(1);
+        if (remaining.length > 0) {
+            if (!operational(springs_map, pos + group, pos + group + 1)){
+                continue;
+            };
 
-        if (remaining.length > 0){
-            arrangement.push('.');
-            for (const remaining_arrangement of possibleArrangements(damaged_groups.slice(1), map_length - arrangement.length)){
-                arrangements.push(arrangement.concat(remaining_arrangement));
-            }
+            arrangements += possibleArrangements(springs_map.slice(pos + group + 1), remaining);
         } else {
-            for (let i=arrangement.length; i< map_length; i++){
-                arrangement.push('.');
-            }
-            arrangements.push(arrangement);
+            if (operational(springs_map, pos + group, springs_map.length)){
+                arrangements += 1;
+            };
         }
     }
     return arrangements;
+
+};
+
+let operational = (spring_map, start, end) => {
+    for (let i = start; i < end; i++) {
+        let spring = spring_map[i];
+        if (spring != '.' && spring != '?') {
+            return false;
+        }
+    }
+    return true;
+}
+let damaged = (spring_map, start, end) => {
+    for (let i = start; i < end; i++) {
+        let spring = spring_map[i];
+        if (spring != '#' && spring != '?') {
+            return false;
+        }
+    }
+    return true;
+}
+
+String.prototype.count = function (char) {
+    let result = 0;
+    for (let i = 0; i < this.length; i++){
+        if (this[i] == char) {
+            result++
+        };
+    }
+    return result;
+}
+
+let sum = function (arr) {
+    return arr.reduce((a, b) => a + b);
 }
